7ce1fb9575d7c9b7911d1b6387ed3894bf3cb587
Chan Samson <christiansamson0102@gmail.com>
2026-07-08 21:15:14 +0800

    08.07.2026

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+const projects = [
+  {
+    id: 1,
+    year: 2026,
+    project: "Car Window Tint Near Me",
+    madeAt: "IBX Digital",
+    builtWith: ["Landing Pages", "WordPress", "Bricks", "ACF"],
+    link: "www.car-window-tint-near-me.com",
+    url: "https://car-window-tint-near-me.com/",
+  },
+  {
+    id: 2,
+    year: 2026,
+    project: "Home and Office Window Tinting",
+    madeAt: "IBX Digital",
+    builtWith: ["Landing Pages", "WordPress", "Elementor", "ACF"],
+    link: "www.homeandofficewindowtinting.com",
+    url: "https://homeandofficewindowtinting.com/",
+  },
+  {
+    id: 3,
+    year: 2026,
+    project: "Ceramic Coating Services",
+    madeAt: "IBX Digital",
+    builtWith: ["Landing Pages", "WordPress", "Elementor", "ACF"],
+    link: "www.ceramic-coating.services",
+    url: "https://ceramic-coating.services/",
+  },
+];
 
-function App() {
-  const [count, setCount] = useState(0)
+const ProjectsTable = () => {
+  const projectList = projects.map((project) => (
+    <tr key={project.id}>
+      <td>{project.year}</td>
+      <td>{project.project}</td>
+      <td>{project.madeAt}</td>
+      <td>
+        <ul className="flex flex-wrap gap-2">
+          {project.builtWith.map((tool) => (
+            <li key={tool}>{tool}</li>
+          ))}
+        </ul>
+      </td>
+      <td>
+        <a href={project.url} target="_blank">
+          {project.link}
+        </a>
+      </td>
+    </tr>
+  ));
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <table className="table-auto w-full text-left">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Project</th>
+          <th>Made at</th>
+          <th>Built with</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>{projectList}</tbody>
+    </table>
+  );
+};
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+export default function Page() {
+  return (
+    <div>
+      <p className="text-left">Christian Samson</p>
+      <h1 className="text-left">All Projects</h1>
+      <ProjectsTable />
+    </div>
+  );
 }
-
-export default App
