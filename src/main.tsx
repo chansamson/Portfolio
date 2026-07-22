2641b1b2f2f40d33e6edb2815edd4e7ebb284f3a
Chan Samson <christiansamson0102@gmail.com>
2026-07-22 20:53:19 +0800

    22.07.2026

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { PageNotFound } from "./components/PageNotFound";
+import { Analytics } from "@vercel/analytics/react";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
@@ -28,6 +29,7 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <RouterProvider router={router} />
+      <Analytics />
     </StrictMode>,
   );
 }
